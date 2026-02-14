4025ae923c553140122bef2258b212904bf59edd
Moinuddin Masud <moinuddinmasud@gmail.com>
2026-02-14 05:27:23 -0800

    some fix

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -56,8 +56,3 @@ output "openai_api_key_secret_name" {
   value       = aws_secretsmanager_secret.openai_api_key.name
   description = "Secrets Manager secret name for OPENAI_API_KEY"
 }
-
-output "summarize_llm_mode_parameter" {
-  value       = aws_ssm_parameter.summarize_llm_mode.name
-  description = "SSM parameter name for summarize_document LLM mode (local or remote)"
-}
