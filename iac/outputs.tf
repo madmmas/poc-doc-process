392ca624206ebe2ce840a223ee2f457d001f74c0
Moinuddin Masud <moinuddinmasud@gmail.com>
2026-02-14 05:21:52 -0800

    some minor fix

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -47,7 +47,12 @@ output "api_gateway_id" {
 #   description = "Website URL for admin web app. For LocalStack, use: http://localhost:4566/admin-web-poc"
 # }
 
-# output "summarize_llm_mode_parameter" {
-#   value       = aws_ssm_parameter.summarize_llm_mode.name
-#   description = "SSM parameter name for summarize_document LLM mode (local or remote)"
-# }
+output "summarize_llm_mode_parameter" {
+  value       = aws_ssm_parameter.summarize_llm_mode.name
+  description = "SSM parameter name for summarize_document LLM mode (local or remote)"
+}
+
+output "openai_api_key_secret_name" {
+  value       = aws_secretsmanager_secret.openai_api_key.name
+  description = "Secrets Manager secret name for OPENAI_API_KEY"
+}
--- iac/provider.tf
+++ iac/provider.tf
@@ -18,7 +18,8 @@ provider "aws" {
     iam          = "http://localhost:4566"
     sts          = "http://localhost:4566"
     dynamodb     = "http://localhost:4566"
-    s3           = "http://localhost:4566"
-    ssm          = "http://localhost:4566"
+    s3              = "http://localhost:4566"
+    ssm             = "http://localhost:4566"
+    secretsmanager  = "http://localhost:4566"
   }
 }
